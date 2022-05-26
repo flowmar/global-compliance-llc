451af372325fcbf0eac0e6510b628bc0f5fd6411
Omar Imam <omarimamdev@gmail.com>
2022-05-26 07:30:34 -0500

    Created and tested the database connection

---
+++ config.js
@@ -0,0 +1,28 @@
+const mysql = require("mysql2");
+
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'globalc',
+  password: 'c0mplianT?',
+  database: 'global_compliance',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// pool.connect(function (err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack)
+//     return
+//   }
+//   console.log('Connected as id ' + connection.threadId);
+// });
+
+
+
+module.exports = {
+  getConnection: (callback) => { 
+    return pool.getConnection(callback);
+  }
+}
--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const PORT = process.env.PORT || 8000;
+const mysql = require("mysql2");
+const db = require("./config");
 
+
+/* Express Setup */
 // Use Express to create the application
 const app = express();
 
-
-
 // Set views
 app.set("views", path.join(__dirname, "views"));
+
 // Set the view engine to use Pug
 app.set("view engine", "pug");
 
@@ -17,6 +20,7 @@ app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname,'public')));
 
 
+/* Routing */
 // Request for root URL
 app.get("/", async (req, res) => {
   const query = await axios.get("https://randomuser.me/api/?results=9");
@@ -25,38 +29,47 @@ app.get("/", async (req, res) => {
   title: "Login"});
 });
 
-// Request for /search URL
+// Request for '/search' URL
 app.get("/search", async (req, res) => {
   res.render("search", {
     title: "Search"
   })
 })
 
-// Request for /add URL
+// Request for '/add' URL
 app.get("/add", async (req, res) => {
   res.render("add", {
     title: "Add"
   })
 })
 
-// Request for /reports URL
+// Request for '/reports' URL
 app.get("/reports", async (req, res) => {
   res.render("reports", {
     title: "Reports"
   })
 })
 
-// Request for /maintenance URL
+// Request for '/maintenance' URL
 app.get("/maintenance", async (req, res) => {
   res.render("maintenance", {
     title: "Maintenance"
   })
 })
 
+db.getConnection(function (err, connection) {
+  // Test MySQL Database Connection
+  connection.query('SELECT * FROM client_schedule.appointments', function (err, results, fields) {
+    if (err) throw err;
+    console.log("Connection successful!");
+    console.log(results);
+    console.log(fields);
+    connection.release();
+  })
+});
 
 
-
-// Set the Port number and display confirmation in console
+// Set the application to listen on a port for requests
 app.listen(PORT, () => {
   console.log(`Listening to requests on http://localhost:${PORT}...`);
 });
