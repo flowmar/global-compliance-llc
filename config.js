b3407db6046b58ece69f733daef29be8cdd30cab
Omar Imam <omarimamdev@gmail.com>
2022-05-28 20:07:08 -0500

    Edit mysql connection for JAWSDB

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
+let connection;
 
 // // Local config
 // const pool = mysql.createPool({
@@ -11,29 +12,17 @@ const mysql = require("mysql");
 //   queueLimit: 0
 // });
 
-// // Config for Heroku
+// Config for Heroku
 // const hpool = mysql.createPool({
 //   host: 'eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 //   user: 'nqz7brmzxbdnzm48',
-//   password: 'hmkikezy9mt00fli',
+//   password: 'ok7qa3sku1bwqony',
 //   port: 3306,
 //   database: 'et2g6mv72e6t4f88',
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0
 // });
-
-const connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query("SELECT * FROM et2g6mv72e6t4f8.Mariners", function (err, rows, fields) {
-  if (err) throw err;
-  
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 
 // pool.connect(function (err) {
 //   if (err) {
@@ -45,9 +34,25 @@ connection.end();
 
 
 
-module.exports = {
-  getConnection: (callback) => {
-    // return pool.getConnection(callback);
-    return connection;
-  }
+// module.exports = {
+//   getConnection: (callback) => {
+//     return pool.getConnection(callback);
+//   }
+// }
+
+// Make connection
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else { 
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'nqz7brmzxbdnzm48',
+    password: 'ok7qa3sku1bwqony',
+    database: 'et2g6mv72e6t4f88'
+  })
 }
+
+connection.connect();
+// Export connection
+module.exports = connection;
